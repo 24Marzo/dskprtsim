f70935573865d88c29fd79330fb0d5775d4daa60
24Marzo <75444397+24Marzo@users.noreply.github.com>
2021-04-30 20:29:47 +0200

    dskprtsim code
    meth

---
+++ dskprt simulator/src/dskprt.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class dskprt {
+	public static void main(String args[ ]) throws InterruptedException {
+	welcome welcomemsg = new welcome();
+	double points = 0;
+	while(5 > 3) {
+		welcomemsg.hellos();
+		System.out.println("\nYour points:" + points);
+		Scanner choice2 = new Scanner(System.in);
+		int choice = choice2.nextInt();
+		switch(choice) {
+		case 1:
+			System.out.println("Wtf how??");
+			Thread.sleep(3000);
+			if (Math.random() < 0.7) {
+			    System.out.println("Your friends didn't appreciate your new pfp. You lost 5 points.");
+				points -= 5;
+				Thread.sleep(3000);
+				continue;
+			}
+			else {
+				System.out.println("Your friends liked your new pfp! You get 3 points.");
+				points += 5;
+				Thread.sleep(3000);
+				continue;
+			}
+			
+		case 2:
+			if (Math.random() < 0.2) {
+			    System.out.println("You went out and you felt sad. You loose 5 points!");
+				points -= 5;
+				Thread.sleep(3000);
+				continue;
+			}
+			else {
+				System.out.println("You went out and found big chungus. Now you know its real! You get 8 points.");
+				points += 8;
+				Thread.sleep(3000);
+				continue;
+			}
+		case 3:
+				System.out.println("It wasn't worth the time, nobody cared. You loose 10 points!");
+				points -= 10;
+				Thread.sleep(3000);
+				break;
+		default:
+				System.out.println("type out something valid, silly");
+				Thread.sleep(3000);
+				continue;
+		}
+	}
+}
+}
+		
+		
+			
+		
+			
+			
+
---
+++ dskprt simulator/src/welcome.java
@@ -0,0 +1,11 @@
+
+public class welcome {
+	void hellos()  throws InterruptedException {
+		System.out.println("Hello welcome to dskprt simulator. Choose what you will do: \n");
+		System.out.println("1. Change profile pic");
+		System.out.println("2. Go out");
+		System.out.println("3. Troll");
+		System.out.println("Become hot anime girl (requires 500 points)");
+	}
+
+}
